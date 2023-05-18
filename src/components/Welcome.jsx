cc0491bec03e88c915fb4a8b643f9cf768dddde8
BlakeBuchholz <blakebuchholz@gmail.com>
2023-05-18 07:54:09 -0500

    Edited image file locations after moving to Miscellaneous sub-folder.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,9 +5,9 @@ const Welcome = () => {
   return (
     <section className='welcome-Section'>
         <video className='welcome-Video'>
-            <source src='./public/DiabloImg/d4welcomebg.mp4' type='video/mp4'/>
+            <source src='./public/DiabloImg/Misc/d4welcomebg.mp4' type='video/mp4'/>
         </video>
-        <img class="D4-Welcome-Logo" src="public\DiabloImg\d4-Welcome-Logo.webp" alt="" />
+        <img class="D4-Welcome-Logo" src="public/DiabloImg/Misc/d4-Welcome-Logo.webp" alt="" />
     <div className='server-Slam'>
         {/* <img className='leather-Background' src="./public/DiabloImg/Leathertexture/leather-texture_800.webp" alt="leather-Background" /> */}
     </div>
