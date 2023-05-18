863f778f248e7580fc46427ad268efe2504a771e
BlakeBuchholz <blakebuchholz@gmail.com>
2023-05-18 07:50:59 -0500

    Edited image file directories after moving images to a new directory, separated Nav Account from the Nav List.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,18 @@ const Header = () => {
                 <div class='company-Logo-Container'>
                 <img class='company-Logo' src='public/DiabloImg/LHRYDNJ5UI2FD.info/Company Logo.png' alt="company-Logo" />
                 </div>
-                <img class='diabloIV-logo' src='public\DiabloImg\diablo-IV-nav-logo.webp' alt="diablo4-nav-Logo" />
-                <ul className='nav-list'>
-                    <li><a href="#">Overview</a></li>
-                    <li><a href="#">Classes</a></li>
-                    <li><a href="#">World</a></li>
-                    <li><a href="#">Story</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Forums</a></li>
-                    <li><a href="#">Account</a></li>
-                    <li><a href="#">Pre-Purchase</a></li>
+                <img class='diabloIV-logo' src='./public/DiabloImg/Misc/diablo-IV-nav-logo.webp' alt="diablo4-nav-Logo" />
+                <ul className='nav-List'>
+                  <li><a href="#">Overview</a></li>
+                  <li><a href="#">Classes</a></li>
+                  <li><a href="#">World</a></li>
+                  <li><a href="#">Story</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Forums</a></li>
+                </ul>
+                <ul className='nav-Account'>
+                  <li><a href="#">Account</a></li>
+                  <li><a className='pre-Purchase-Button' href="#">Pre-Purchase</a></li>
                 </ul>
             </div>
         </div>
