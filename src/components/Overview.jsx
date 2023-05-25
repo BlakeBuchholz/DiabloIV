7cd0d2e66b13f9e501683ada298d2dff5f48d603
BlakeBuchholz <blakebuchholz@gmail.com>
2023-05-24 23:42:44 -0500

    Initialize Overview component.

---
+++ src/components/Overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <div className='overview'>
+        <img class="overview-Background" src="public\DiabloImg\Leathertexture\leather-texture_800.webp" alt="leather-Texture" />
+    </div>
+  )
+}
+
+export default Overview
