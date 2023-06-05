3e98980806a24b991dc8b58c539945a5b708855a
BlakeBuchholz <blakebuchholz@gmail.com>
2023-06-05 17:55:43 -0500

    Added new background video to resemble D4's launch site.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Welcome = () => {
   return (
-    <section className='welcome-Section'>
-        <video className='welcome-Video'>
-        <source src="\DiabloImg\Misc\d4-Welcome-BG.mp4" type="video/MP4" autoplay loop muted
-        />
-        Sorry, your browser doesn't support videos.
+    <div>
+      <section className='welcome-Section'>
+        <video className='welcome-Video' autoPlay loop muted>
+          <source src="./DiabloImg/Misc/d4-Launch-Masthead.mp4" type="video/mp4" />
+          Sorry, your browser doesn't support videos.
         </video>
-        <img class="D4-Welcome-Logo" src="public/DiabloImg/Misc/d4-Welcome-Logo.webp" alt="" />
-    </section>
-  )
-}
+        <img className="D4-Welcome-Logo" src="DiabloImg/Misc/d4-Welcome-Logo.webp" alt="" />
+      </section>
+      <div className='pre-Purchase'>
+        <h1>WELCOME TO HELL</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Welcome
+export default Welcome;
